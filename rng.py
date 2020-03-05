99a7a2849936be07a75600606412ea3e2169d057
Jack Schefer <jack.schefer9@gmail.com>
2020-03-05 13:23:10 -0500

    add gitignore, rng skeleton and testing code

---
+++ rng.py
@@ -0,0 +1,21 @@
+def rng():
+    # TODO implmenent rng
+    return 0.5
+
+def rvg():
+    # TODO implmenent rvg
+    return 0.5
+
+
+if __name__ == '__main__':
+    '''
+    Do testing relating to expected first three terms, then print out the required terms.
+    '''
+    random_nums = [rng() for i in range(55)]
+    assert abs(random_nums[0] - 0.6779) < 0.0001, 'First term ({}) incorrect'.format(random_nums[0])
+    assert abs(random_nums[1] - 0.1701) < 0.0001, 'Second term ({}) incorrect'.format(random_nums[1])
+    assert abs(random_nums[2] - 0.5096) < 0.0001, 'Third term ({}) incorrect'.format(random_nums[2])
+
+    print('u_51:', random_nums[50]) # index is one lower because of zero based indexing
+    print('u_52:', random_nums[51])
+    print('u_53:', random_nums[52])
